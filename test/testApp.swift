5a25b22f9e151b86c16e86e20cb1cbdeb8a46315
Octavio Lara <octavioasapchi@gmail.com>
2025-03-26 19:53:40 -0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Octavio Lara on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
