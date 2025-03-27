c2d3bdafe1a8fadbd9a708672593f966cc80fc9b
Octavio Lara <octavioasapchi@gmail.com>
2025-03-27 12:59:00 -0300

    ✨ feat: enhance ItemModel with immutable struct and completion update functionality

--- test/Models/ItemModel.swift
+++ test/Models/ItemModel.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
+
+// Immutable Struct
 struct ItemModel: Identifiable {
-    let id = UUID()
+    let id: String
     let title: String
     let isCompleted: Bool
+    init(title: String, isCompleted: Bool, id: String = UUID().uuidString){
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(
+            title: self.title,
+            isCompleted: !self.isCompleted,
+            id: self.id
+        )
+    }
 }
