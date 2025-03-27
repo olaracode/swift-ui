2f4f1d4cee4265eaba49dca5602d488656915480
Octavio Lara <octavioasapchi@gmail.com>
2025-03-27 02:05:41 -0300

    ✨ feat: implement item deletion and reordering in ListView

--- test/Views/ListView.swift
+++ test/Views/ListView.swift
@@ -14,14 +14,26 @@ struct ListView: View {
         ItemModel(title: "Third Todo", isCompleted: true),
     ]
     var body: some View {
-        List(items) { item in
-            ListItemView(item: item)
+        List {
+            ForEach(items) { item in
+                ListItemView(item: item)
+            }
+            .onDelete(perform: deleteItem)
+            .onMove(perform: moveItem)
+            
         }
         .navigationTitle("Todo List")
         .navigationBarItems(
             leading: EditButton(),
             trailing: NavigationLink("Add", destination: AddView())
         )
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    func moveItem(from: IndexSet, to: Int){
+        items.move(fromOffsets: from, toOffset: to)
     }
 }
 
