d7434c88a55fd7ca3c6c42ba89381afc557546f8
Octavio Lara <octavioasapchi@gmail.com>
2025-03-26 23:22:25 -0300

    add: Card-UI design

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.gray)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("buenos-aires")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack {
+                    Text("Buenos Aires")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    // The spacer takes all the available space
+                    // works to space elements between
+                    Spacer()
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(4.5/5)")
+                            .foregroundColor(.black)
+                    
+                            .fontWeight(.bold)
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+
+
+                }
+                Text("Come visit the most beautiful port city on south america.")
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+                
+            }
+            .padding()
+            .background(
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+            )
+            .padding()
+            
         }
-        .padding()
     }
 }
 
