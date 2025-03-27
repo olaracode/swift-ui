4c7fddfc1b08230ca6f385eefd05fc4cb8be34fc
Octavio Lara <octavioasapchi@gmail.com>
2025-03-27 00:21:43 -0300

    ✨ refactor game logic to use @State for player and CPU states

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // User states
+    @State var playerCard = "card7"
+    @State var playerScore = 0
     
-    var playerCard = "card7"
-    var playerScore = 0
-    
-    var cpuCard = "card13"
-    var cpuScore = 0
+    // CPU States
+    @State var cpuCard = "card13"
+    @State var cpuScore = 0
     
     var body: some View {
         ZStack{
@@ -78,7 +79,26 @@ struct ContentView: View {
 
 
     func deal(){
-        print("deal")
+        // generate random values
+        let playerValue = Int.random(in:2...14)
+        let cpuValue = Int.random(in:2...14)
+        
+        // update states
+        playerCard = "card" + String(playerValue)
+        cpuCard = "card" + String(cpuValue)
+        
+        // update the scores
+        if playerValue > cpuValue {
+            
+            // add 1 to player score
+            playerScore += 1
+        }
+        else if cpuValue > playerValue {
+            // add 1 to cpu score
+            cpuScore += 1
+        } else {
+            // tie
+        }
     }
 }
 
