1654acb9095048ca0252e38edbca852a3e245915
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 18:25:52 -0300

    ✨ refactor: replace inline user display with ProfileView component in ContentView

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,31 +13,7 @@ struct ContentView: View {
     @State private var cached: Bool = false
     @Query() var githubUsers: [GithubUser]
     var body: some View {
-        VStack(spacing: 20) {
-            AsyncImage(url: URL(string: githubUsers.first?.avatarUrl ?? "")) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-            } placeholder: {
-                Circle()
-                    .foregroundColor(.secondary)
-                                        
-            }
-            .frame(width: 128, height: 120)
-
-           
-            Text(githubUsers.first?.login ?? "Login Placeholder ...")
-                .bold()
-                .font(.title3)
-            Text(githubUsers.first?.bio ?? "Bio placeholder ...")
-                .padding()
-            if(cached){
-                Text("Cached Value")
-                    .font(.caption2)
-            }
-            Spacer()
-        }
+        ProfileView(githubUser: githubUsers.first)
         .padding()
         // Adds asyncronous task
         .task {
