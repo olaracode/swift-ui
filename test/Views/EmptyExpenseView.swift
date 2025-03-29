e05304a5e66a13bda27e58ad226967b342bd5c68
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 10:51:09 -0300

    ✨ add: EmptyExpenseView

---
+++ test/Views/EmptyExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyExpenseView.swift
+//  test
+//
+//  Created by Octavio Lara on 29/03/2025.
+//
+
+import SwiftUI
+
+struct EmptyExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyExpenseView()
+}
