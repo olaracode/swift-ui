0a79ef3b499591c3a696cc14f2f03885995337a3
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 10:07:06 -0300

    ✨ add: implement ModelContainer for Expense model in testApp

--- test/testApp.swift
+++ test/testApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
-
+import SwiftData
 @main
 struct testApp: App {
+    // declarative container
+    let container: ModelContainer = {
+        let schema = Schema([Expense.self])
+        let config = ModelConfiguration()
+        let container = try! ModelContainer(for: schema, configurations: config)
+        return container
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(container)
+        // TO store several values you'd use:
+        // .modelContainer(for: [...])
+//        .modelContainer(for: Expense.self)
     }
 }
