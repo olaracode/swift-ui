a73c5973fc23591b54b8d04a369d5e9bfcf9b607
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 10:07:24 -0300

    ✨ add: implement ContentView to display and manage expenses with add and edit functionality

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -6,16 +6,59 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @State private var isShowingItemSheet = false
+    @State private var expenseToEdit: Expense?
+    
+    @Query(sort: \Expense.date) var expenses: [Expense]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses) { expense in
+                    ExpenseCell(expense: expense)
+                        .onTapGesture {
+                            expenseToEdit = expense
+                        }
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        context.delete(expenses[index])
+                    }
+                }
+            }
+            .navigationTitle("Expenses")
+            .navigationBarTitleDisplayMode(.large)
+            .sheet(isPresented: $isShowingItemSheet) {
+                AddExpenseSheet()
+            }
+            .sheet(item: $expenseToEdit) { expense in
+                UpdateExpenseSheet(expense: expense)
+            }
+            .toolbar {
+                if !expenses.isEmpty {
+                    Button("Add Expense", systemImage: "plus") {
+                        isShowingItemSheet = true
+                    }
+                }
+            }
+            .overlay {
+                if expenses.isEmpty {
+                    ContentUnavailableView(label: {
+                        Label("No Expenses", systemImage: "list.bullet.rectangle.portrait")
+                    }, description: {
+                        Text("Start adding expenses to see your list")
+                    }, actions: {
+                        Button("Add Expense") {
+                            isShowingItemSheet = true
+                        }
+                    })
+                    .offset(y: -60)
+                }
+            }
         }
-        .padding()
     }
 }
 
