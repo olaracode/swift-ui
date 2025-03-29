bb61b4526b1f037546b66a9c238dc7f05797e738
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 18:09:30 -0300

    ✨ add: implement asynchronous fetching of Github user data with caching in ContentView

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -6,16 +6,65 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    
+    @State private var cached: Bool = false
+    @Query() var githubUsers: [GithubUser]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            AsyncImage(url: URL(string: githubUsers.first?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+                                        
+            }
+            .frame(width: 128, height: 120)
+
+           
+            Text(githubUsers.first?.login ?? "Login Placeholder ...")
+                .bold()
+                .font(.title3)
+            Text(githubUsers.first?.bio ?? "Bio placeholder ...")
+                .padding()
+            if(cached){
+                Text("Cached Value")
+                    .font(.caption2)
+            }
+            Spacer()
         }
         .padding()
+        // Adds asyncronous task
+        .task {
+            if githubUsers.first != nil {
+                cached = true
+                return
+            }
+            do {
+                let apiUser = try await GithubApi().getUser(username: "olaracode")
+                let githubUser = GithubUser(
+                    login: apiUser.login,
+                    avatarUrl: apiUser.avatarUrl,
+                    bio: apiUser.bio
+                )
+                context.insert(githubUser)
+                
+            } catch GithubError.invalidURL {
+                print("Invalid Url")
+                //
+            } catch GithubError.invalidResponse {
+                print("Invalid Response")
+            } catch GithubError.invalidData {
+                print("Invalid Data")
+            } catch {
+                print("Unhandled unknown error")
+            }
+        }
     }
 }
 
