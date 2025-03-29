20d9a219b5f44c9339a8d7b246e930234ee53a0e
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 18:09:01 -0300

    ✨ add: create GithubModel and GithubApi files

---
+++ test/Model/GithubModel.swift
@@ -0,0 +1,8 @@
+//
+//  GithubModel.swift
+//  test
+//
+//  Created by Octavio Lara on 29/03/2025.
+//
+
+import Foundation
---
+++ test/Utils/GithubApi.swift
@@ -0,0 +1,8 @@
+//
+//  GithubApi.swift
+//  test
+//
+//  Created by Octavio Lara on 29/03/2025.
+//
+
+import Foundation
