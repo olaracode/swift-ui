c33c294f3af5ace4608450f495ab3235878e60f8
Octavio Lara <octavioasapchi@gmail.com>
2025-03-29 10:07:13 -0300

    ✨ add: implement AddExpenseSheet view for adding new expenses with name, date, and value

--- test/Views/AddExpenseSheet.swift
+++ test/Views/AddExpenseSheet.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct AddExpenseSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @State private var name: String = ""
+    @State private var date: Date = .now
+    @State private var value: Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Expense Name", text: $name)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                // For Some reason working with USD didn't work
+                TextField("Value", value: $value, format: .currency(code: "ARS"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add Expense")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading){
+                    Button("Cancel") { dismiss() }
+                }
+                ToolbarItemGroup(placement: .topBarTrailing){
+                    Button("Save") {
+                        let expense = Expense(name: name, date: date, value: value)
+                        // this will save automatically
+                        context.insert(expense)
+                        
+                        // to force a save
+                        // try! context.save()
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
