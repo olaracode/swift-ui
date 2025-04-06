be2de13c7a72b9029e3c0f00b7bd581e570bec61
Octavio Lara <octavioasapchi@gmail.com>
2025-04-06 20:59:26 -0300

    ✨ add: refactor AuthManager to use Keychain for token storage and implement user fetching logic

--- test/ViewModel/AuthManager.swift
+++ test/ViewModel/AuthManager.swift
@@ -5,14 +5,43 @@
 //  Created by Octavio Lara on 06/04/2025.
 //
 
-import SwiftUI
+import Foundation
+import KeychainAccess
 
-struct AuthManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class AuthManager: ObservableObject {
+    @Published var isAuthenticated: Bool = false
+    @Published var user: User? = nil
+
+    private let keychain = Keychain(service: "com.yourapp.token")
+
+    var token: String? {
+        keychain["authToken"]
     }
-}
 
-#Preview {
-    AuthManager()
+    func login(withToken token: String, user: User) {
+        keychain["authToken"] = token
+        self.isAuthenticated = true
+        self.user = user
+    }
+
+    func logout() {
+        keychain["authToken"] = nil
+        self.user = nil
+        self.isAuthenticated = false
+    }
+
+    func fetchUser() async {
+        guard let token = token else { return }
+        print(token)
+        do {
+            let user = try await AuthApi.getUser(token: token)
+            print(user)
+            self.user = User(email: user.email, name: user.name, id: user._id)
+            self.isAuthenticated = true
+        } catch {
+            print("Failed to fetch", error)
+        }
+                // Example API call to get user data
+       
+    }
 }
