100265b4b51aa0e7f08e9009dcc0e1cba5803074
Octavio Lara <octavioasapchi@gmail.com>
2025-04-08 17:12:57 -0300

    ✨ add: implement authentication API methods for login, registration, and user retrieval; refactor Api structure

--- test/Lib/Api/Api+Auth.swift
+++ test/Lib/Api/Api+Auth.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+extension Api {
+    private struct Endpoints {
+        static let login = "/v1/auth/login"
+        static let register = "/v1/auth/register"
+        static let me = "/v1/auth/me"
+    }
+    static func login(loginBody: LoginBody)async throws -> RegisterLoginResponse{
+        do {
+            let response: RegisterLoginResponse = try await fetch.post(
+                endpoint: Endpoints.login,
+                payload: loginBody
+            )
+            return response
+        }
+        catch ApiError.unauthenticated {
+            throw AuthRequestError.invalidEmailOrPassword
+        }
+        catch {
+            throw ApiError.serverError
+        }
+    }
+    
+    static func register(body: RegisterBody) async throws -> RegisterLoginResponse {
+        do {
+            let response: RegisterLoginResponse = try await fetch.post(endpoint: "/v1/auth/register", payload: body)
+            return response
+        } catch {
+            throw ApiError.serverError
+        }
+    }
+    static func getUser(
+        token: String
+    ) async throws -> UserResponse {
+        let response: UserResponse = try await fetch.get(endpoint: Endpoints.me, token: token)
+        return response
+    }
+}
