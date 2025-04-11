943e641db0bebb66e7eec7aec9e4127130624b8e
Octavio Lara <octavioasapchi@gmail.com>
2025-04-10 21:39:58 -0300

    ✨ add: implement PlantManager class to manage plant data; include methods for fetching, adding, and cleaning plants

--- test/ViewModel/PlantManager.swift
+++ test/ViewModel/PlantManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class PlantManager: ObservableObject {
+    @Published var plants: [PlantModel]
+    init(){
+       plants = []
+    }
+    
+    func getPlants(token: String) async throws {
+        let apiPlants = try await Api.getPlants(token: token)
+        DispatchQueue.main.async {
+            self.plants.append(contentsOf: apiPlants)
+        }
+    }
+    
+    func addPlant(payload: PlantBody, token: String) async throws{
+        let newPlant = try await Api.createPlant(payload: payload, token: token)
+        DispatchQueue.main.async {
+            self.plants.append(newPlant)
+        }
+    }
+    
+    func clean(){
+        plants = []
+    }
+}
