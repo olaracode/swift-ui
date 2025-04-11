3fd7c9009645a91ce935245306de65c16896f10f
Octavio Lara <octavioasapchi@gmail.com>
2025-04-10 21:38:43 -0300

    ✨ add: implement ProtectedView to manage authenticated content display; show unauthenticated message when user is not authenticated

--- test/Views/ProtectedView.swift
+++ test/Views/ProtectedView.swift
@@ -5,4 +5,17 @@
 //  Created by Octavio Lara on 08/04/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProtectedView<Content: View>: View {
+    @EnvironmentObject var auth: AuthManager
+    var content: () -> Content
+    
+    var body: some View {
+        if auth.isAuthenticated {
+            content()  // Show content if authenticated
+        } else {
+            Text("Unauthenticated")  // Show unauthenticated message if no user
+        }
+    }
+}
