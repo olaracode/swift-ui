346e2b65dfb200bea868bacc84d77f062ef3a77f
Octavio Lara <octavioasapchi@gmail.com>
2025-04-10 21:40:26 -0300

    ✨ add: implement API methods for fetching and creating plants; define PlantResponse and PlantBody models

--- test/Lib/Api/Api+Plants.swift
+++ test/Lib/Api/Api+Plants.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+
+
+extension Api {
+    private struct Endpoints {
+        static let plants = "/v1/plants"
+    }
+    
+    static func getPlants(token: String) async throws -> [PlantModel] {
+        let response: [PlantResponse] = try await fetch.get(
+            endpoint: Endpoints.plants, token: token
+        )
+        print(response)
+        return response.map {PlantModel(from: $0)}
+    }
+    static func createPlant(payload: PlantBody ,token: String) async throws -> PlantModel {
+        print("CREATING PLANT ---------")
+        print("🌱 Payload: \(payload)")
+        let response: PlantResponse = try await fetch.post(
+            endpoint: Endpoints.plants,
+            payload: payload,
+            token: token
+        )
+        print("🛜 Response: \(response)")
+        return PlantModel(from: response)
+    }
+}
+
+
+struct PlantResponse: Codable {
+    let _id: String
+    let name: String
+    let light: String
+    let weatherType: String
+    let status: String
+    let location: String
+    let lastWatered: String
+    let user: String
+}
+
+struct PlantBody: Codable {
+    let name: String
+    let weatherType: String
+    let light: String
+    let status: String
+    let location: String
+    let lastWatered: String
+}
