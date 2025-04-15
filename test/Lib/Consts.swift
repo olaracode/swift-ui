0be602592c6c732f95627747a07422f16efa9606
Octavio Lara <octavioasapchi@gmail.com>
2025-04-15 00:45:49 -0300

    ✨ feat: add PlantModel placeholder with sample data for testing

--- test/Lib/Consts.swift
+++ test/Lib/Consts.swift
@@ -13,3 +13,18 @@ struct AppMetadata {
 }
 
 
+struct Placeholders {
+    static let plant: PlantModel = PlantModel(
+        id: "1234",
+        name: "Putica",
+        light: "Mucha",
+        weatherType: .dry,
+        status: .inTreatment,
+        location: .indoor,
+        lastWatered: "Two hours ago",
+        nextWatering: Date.now,
+        wateringIntervalHours: 24,
+        careNote: "Care note testa",
+        user: "123123123"
+    )
+}
