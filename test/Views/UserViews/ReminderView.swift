78ba301e7077f7c6432166a75f5dc5479b1a5d06
Octavio Lara <octavioasapchi@gmail.com>
2025-04-29 23:16:39 -0300

    ✨ feat: add ReminderView for displaying plant notifications

---
+++ test/Views/UserViews/ReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderView.swift
+//  test
+//
+//  Created by Octavio Lara on 19/04/2025.
+//
+
+import SwiftUI
+
+struct ReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderView()
+}
